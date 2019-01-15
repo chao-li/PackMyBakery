c9d2a9671af097e7b72d6f798df06ff92e12a3c9
Chao Li <chao.li.mobile.dev@gmail.com>
2019-01-16 06:41:34 +1100

    calculation done in async task

--- app/src/main/java/com/clidev/packmybakery/MainActivity.java
+++ app/src/main/java/com/clidev/packmybakery/MainActivity.java
@@ -2,14 +2,21 @@ package com.clidev.packmybakery;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.blueberry_muffin_et) EditText mBlueberryMuffinEt;
     @BindView(R.id.croissant_et) EditText mCroissantEt;
     @BindView(R.id.place_order_button) Button mPlaceOrderButton;
+    @BindView(R.id.loading_bar) ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,59 +72,48 @@ public class MainActivity extends AppCompatActivity {
                     mCroissantNumber = 0;
                 }
 
+                // turn on loading icon
+                mProgressBar.setVisibility(View.VISIBLE);
+
+
                 // Calculate the optimal combinations
-                ArrayList<Integer> vegemitePackage = PackageLooper.calculateVegemite(mVegemiteNumber);
-                ArrayList<Integer> blueberryPackage = PackageLooper.calculateBlueberry(mBlueberryNumber);
-                ArrayList<Integer> croissantPackage = PackageLooper.calculateCroissant(mCroissantNumber);
-
-                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
-                intent.putIntegerArrayListExtra("VEGEMITE", vegemitePackage);
-                intent.putIntegerArrayListExtra("BLUEBERRY", blueberryPackage);
-                intent.putIntegerArrayListExtra("CROISSANT", croissantPackage);
-                startActivity(intent);
-
-
-                /*
-                if (vegemitePackage.size() >= 1) {
-                    Timber.d("///////////////////VEGEMITE //////////////////");
-                    Timber.d("small (3) package is: " + vegemitePackage.get(0));
-                    Timber.d("medium (5) package is: " + vegemitePackage.get(1));
-
-
-                } else {
-                    Timber.d("///////////////////BLUEBERRY //////////////////");
-                    Timber.d("Optimal blueberry package method not found");
-                }
-
-
-
-                if (blueberryPackage.size() >= 1) {
-                    Timber.d("///////////////////BLUEBERRY //////////////////");
-                    Timber.d("small (2) package is: " + blueberryPackage.get(0));
-                    Timber.d("medium (5) package is: " + blueberryPackage.get(1));
-                    Timber.d("large (8) package is: " + blueberryPackage.get(2));
-
-                } else {
-                    Timber.d("///////////////////BLUEBERRY //////////////////");
-                    Timber.d("Optimal blueberry package method not found");
-                }
-
-
-                if (croissantPackage.size() >= 1) {
-                    Timber.d("///////////////////CROISSANT //////////////////");
-                    Timber.d("small (3) package is: " + croissantPackage.get(0));
-                    Timber.d("medium (5) package is: " + croissantPackage.get(1));
-                    Timber.d("large (9) package is: " + croissantPackage.get(2));
-
-                } else {
-                    Timber.d("///////////////////CROISSANT //////////////////");
-                    Timber.d("Optimal croissant package method not found");
-                }
-                */
-
+                new CalculateTask().execute();
 
             }
         });
 
+    }
+
+
+    public class CalculateTask extends AsyncTask<Void, Void,  ArrayList<ArrayList<Integer>>> {
+
+
+        @Override
+        protected ArrayList<ArrayList<Integer>> doInBackground(Void... voids) {
+            ArrayList<Integer> vegemitePackage = PackageLooper.calculateVegemite(mVegemiteNumber);
+            ArrayList<Integer> blueberryPackage = PackageLooper.calculateBlueberry(mBlueberryNumber);
+            ArrayList<Integer> croissantPackage = PackageLooper.calculateCroissant(mCroissantNumber);
+
+            ArrayList<ArrayList<Integer>> allPackage = new ArrayList<>();
+            allPackage.add(vegemitePackage);
+            allPackage.add(blueberryPackage);
+            allPackage.add(croissantPackage);
+
+            return allPackage;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<ArrayList<Integer>> arrayLists) {
+            mProgressBar.setVisibility(View.INVISIBLE);
+            ArrayList<Integer> vegemitePackage = arrayLists.get(0);
+            ArrayList<Integer> blueberryPackage = arrayLists.get(1);
+            ArrayList<Integer> croissantPackage = arrayLists.get(2);
+
+            Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+            intent.putIntegerArrayListExtra("VEGEMITE", vegemitePackage);
+            intent.putIntegerArrayListExtra("BLUEBERRY", blueberryPackage);
+            intent.putIntegerArrayListExtra("CROISSANT", croissantPackage);
+            startActivity(intent);
+        }
     }
 }
